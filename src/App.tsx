64f482dea94bc0e1faac5eb0fc6c56cc07a4f0bd
Elad Shaham <7040645+eshaham@users.noreply.github.com>
2022-02-01 11:00:57 +0200

    add screen responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Box } from '@mui/material';
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import { Box, ThemeProvider } from '@mui/material';
 
 import Home from './containers/Home';
 import Setup from './containers/Setup';
 import Settings from './containers/Settings';
 
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
+
 function App() {
   return (
-    <Box minHeight="100vh">
-      <Router>
-        <Route path="/" exact component={Home} />
-        <Route path="/setup" component={Setup} />
-        <Route path="/settings" component={Settings} />
-      </Router>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box minHeight="100vh">
+        <Router>
+          <Route path="/" exact component={Home} />
+          <Route path="/setup" component={Setup} />
+          <Route path="/settings" component={Settings} />
+        </Router>
+      </Box>
+    </ThemeProvider>
   );
 }
 
